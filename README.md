dc29a36fe752ae4be8d23aefdcd6239f263119ad
TonyS <tonysilvasd@gmail.com>
2018-11-26 08:42:30 -0800

    add initial project files

---
+++ README.md
@@ -0,0 +1,5 @@
+Hey There! :smile:
+
+This is my San Diego Top Spots app that lists a bunch a different cool spots around San Diego to visit.
+
+To get started, open the app in VS code, or code editor of your choosing, and install the dependencies via the terminal by typing '$ npm install'. Next, in the terminal, type '$ static .' to start the app on the local node server and open the localhost:8080 in the browser of your choice.
--- main.js
+++ main.js
@@ -1,4 +1,30 @@
 $(document).ready(function() {
 // write your code here
-
+$.getJSON("data.json", function (data){
+ var tr = {};
+ console.log(data);
+ //for each element in each array, post the data(td) to the table rows(tr)
+ for (var i = 0; i < data.length; i++) {
+    tr = $('<tr>');
+    tr.append("<td>" + data[i].name + "</td>");
+    tr.append("<td>" + data[i].description + "</td>");
+    //Line below has an anchor tab that adds a link to the button that will take the user to google maps with the coordinates
+    tr.append("<td><a href='https://www.google.com/maps?q=" + data[i].location + "' target='_blank'><button class='button'>Open in Google Maps</button></a></td>");
+    $('.Top-spots-list').append(tr);
+ };
+})
 });
+
+
+
+/* getJSON from Jquery site
+$.each( data, function( key, val) {
+        items.push("li id='" + key + "'>" + val + "</li>");
+    });
+
+    $( "<ul/>", {
+        "class": "my-new-list",
+        html: items.join( "" )
+      }).appendTo( "body" );
+    console.log(data);
+    */
